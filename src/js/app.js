fa87393660e4301a47267d0b0532e467c30a8975
marianospi123 <141750385+marianospi123@users.noreply.github.com>
2023-10-20 02:24:29 +0000

    profile

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,34 @@
+import "../style/index.css";
 
 function render(variables = {}) {
   console.log("Estas son las variables actuales: ", variables);
 
-
-  const cover = variables.includeCover ? `<div class="cover"><img src="${variables.background}" /></div>` : '<div class="cover"></div>';
+  const cover = variables.includeCover
+    ? `<div class="cover"><img src="${variables.background}" /></div>`
+    : '<div class="cover"></div>';
 
   const socialMediaLinks = `
     <ul class="${variables.socialMediaPosition}">
-      ${variables.twitter ? `<li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>` : ''}
-      ${variables.github ? `<li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>` : ''}
-      ${variables.linkedin ? `<li><a href="https://es.linkedin.com/"${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>` : ''}
-      ${variables.instagram ? `<li><a href="https://www.instagram.com/"${variables.instagram}"><i class="fab fa-instagram"></i></a></li>` : ''}
+      ${
+        variables.twitter
+          ? `<li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>`
+          : ""
+      }
+      ${
+        variables.github
+          ? `<li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>`
+          : ""
+      }
+      ${
+        variables.linkedin
+          ? `<li><a href="https://es.linkedin.com/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>`
+          : ""
+      }
+      ${
+        variables.instagram
+          ? `<li><a href="https://www.instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>`
+          : ""
+      }
     </ul>
   `;
 
@@ -18,17 +36,15 @@ function render(variables = {}) {
     <div class="widget">
       ${cover}
       <img src="${variables.avatarURL}" class="photo" />
-      <h1>${variables.name || ''} ${variables.lastName || ''}</h1>
-      <h2>${variables.role || ''}</h2>
-      <h3>${variables.city || ''}, ${variables.country || ''}</h3>
+      <h1>${variables.name || ""} ${variables.lastName || ""}</h1>
+      <h2>${variables.role || ""}</h2>
+      <h3>${variables.city || ""}, ${variables.country || ""}</h3>
       ${socialMediaLinks}
     </div>
   `;
 
-
   document.querySelector("#widget_content").innerHTML = profileCard;
 }
-
 
 const initialVariables = {
   includeCover: true,
@@ -46,14 +62,12 @@ const initialVariables = {
   country: null
 };
 
-
 render(initialVariables);
 
-
-document.querySelectorAll(".picker").forEach(function (elm) {
-  elm.addEventListener("change", function (e) {
-      const attribute = e.target.getAttribute("for");
-      initialVariables[attribute] = this.value;
-      render(initialVariables);
+document.querySelectorAll(".picker").forEach(function(elm) {
+  elm.addEventListener("change", function(e) {
+    const attribute = e.target.getAttribute("for");
+    initialVariables[attribute] = this.value;
+    render(initialVariables);
   });
 });
